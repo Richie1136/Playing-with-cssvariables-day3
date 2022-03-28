e8e18e825e7c04dec3ff6007c46251e4382947d3
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-03-28 13:44:45 -0400

    Inside of the index.html file i added a property of data sizing to the spacing and blur inputs and then inside of the main.js file i grbbed those and then i set the property to the input name and then added the suffix to whatever the value would be when i move the range on the input field

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 const spacing = document.querySelector('#spacing')
 const blur = document.querySelector('#blur')
 const base = document.querySelector('#base')
+
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || ''
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+  console.log(suffix)
+}
+
+blur.addEventListener('mousemove', handleUpdate)
+blur.addEventListener('change', handleUpdate)
+spacing.addEventListener('mousemove', handleUpdate)
+spacing.addEventListener('change', handleUpdate)
+base.addEventListener('mousemove', handleUpdate)
+base.addEventListener('change', handleUpdate)
